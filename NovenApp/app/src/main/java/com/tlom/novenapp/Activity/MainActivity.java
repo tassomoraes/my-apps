2776e348befc53b61968f5540cdd3928903fd4cd
Tasso Moraes <tassoluis@gmail.com>
2021-04-08 11:37:30 -0300

    Novena de São José Activity created

--- NovenApp/app/src/main/java/com/tlom/novenapp/Activity/MainActivity.java
+++ NovenApp/app/src/main/java/com/tlom/novenapp/Activity/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -51,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
                         new RecyclerItemClickListener.OnItemClickListener() {
                             @Override
                             public void onItemClick(View view, int position) {
-                                
+                                Intent intent = new Intent(getApplicationContext(),SaoJoseActivity.class);
+                                startActivity(intent);
                             }
 
                             @Override
